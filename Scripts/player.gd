242b2d22d7819bd838e30e19410db77b1e40c0e9
Sam Cavalheiro <samcava39@gmail.com>
2026-02-25 18:04:09 -0300

    Adiciona e implementa animação idle e run do gambá

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 @export var speed :float = 5.0
 #@export var jump_velocity :float = 4.5
 
+@onready var animated_sprite := $AnimatedSprite3D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -19,9 +21,13 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		animated_sprite.play("run")
+		if direction.x:
+			animated_sprite.flip_h = direction.x < 0
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
 	else:
+		animated_sprite.play("idle")
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 
